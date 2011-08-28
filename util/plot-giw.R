2851263dc41c661a2b70c9a32b7c12a878cda53d
Peter Kleiweg <p.c.j.kleiweg@rug.nl>
2011-08-28 15:35:58 +0200

    [plot] with GIW: recover from error when asymptotic plot fails

--- util/plot-giw.R
+++ util/plot-giw.R
@@ -4,6 +4,28 @@ library(iL04)
 dif <- as.vector(read.dif('../diff/diff.txt'))
 geo <- as.vector(read.dif('tmp.dst'))
 
+o <- order(geo)
+dif <- dif[o]
+geo <- geo[o]
+
+xrange <- range(geo)
+
+x <- seq(xrange[1], xrange[2], length.out = 200)
+
+step <- max(geo) / 50.0
+g1 <- rep(NA, 50)
+d1 <- rep(NA, 50)
+n <- 0
+for (i in 1:50) {
+  idx = geo > step * (i - 1) & geo <= step * i
+  if (length(idx[idx]) > 0) {
+    n <- n + 1
+    g1[n] <- mean(geo[idx])
+    d1[n] <- mean(dif[idx])
+  }
+}
+
+
 con <- file('../description', open='rt', encoding='UTF-8')
 desc <- readLines(con, n=1)
 close(con)
@@ -16,73 +38,70 @@ desc <- gsub('&amp;', '&', desc, fixed=TRUE)
 postscript(file='plot%02d.eps', width=6, height=5, onefile=FALSE, horizontal=FALSE, paper='a4')
 par(mar=c(5, 4, 1, 2) + 0.1)
 
+assign("needplot", TRUE, envir=.GlobalEnv)
 
-o <- order(geo)
-dif <- dif[o]
-geo <- geo[o]
+plot01 <- function() {
 
-a <- min(dif)
-b <- max(dif) - a
-c <- geo[length(dif[dif < mean(dif)])]
+  a <- min(dif)
+  b <- max(dif) - a
+  c <- geo[length(dif[dif < mean(dif)])]
 
-m <- nls(dif ~ a + b * geo / (c + geo), start=list(a=a, b=b, c=c), trace=TRUE)
+  m <- nls(dif ~ a + b * geo / (c + geo), start=list(a=a, b=b, c=c), trace=TRUE)
 
-a <- as.numeric(m$m$getPars()[1])
-b <- as.numeric(m$m$getPars()[2])
-c <- as.numeric(m$m$getPars()[3])
+  a <- as.numeric(m$m$getPars()[1])
+  b <- as.numeric(m$m$getPars()[2])
+  c <- as.numeric(m$m$getPars()[3])
 
-xrange <- range(geo)
-yrange <- c(a - b, max(max(dif), a + b))
-
-x <- seq(xrange[1], xrange[2], length.out = 200)
-
-abc <- sprintf('a = %.3g   b = %.3g   c = %g', a, b, round(c))
+  abc <- sprintf('a = %.3g   b = %.3g   c = %g km', a, b, round(c))
 
 
-plot(xrange, yrange, xlab='Geograpic distance (km)', ylab='Linguistic difference', type='n', sub=abc)
-points(geo, dif, col='#A0A0A0', pch='.')
+  yrange <- c(a - b, max(max(dif), a + b))
+  plot(xrange, yrange, xlab='Geograpic distance (km)', ylab='Linguistic difference', type='n', sub=abc)
+  assign("needplot", FALSE, envir=.GlobalEnv)
+  points(geo, dif, col='#A0A0A0', pch='.')
 
-#if (length(geo) < 5000) {
-#  ml <- loess(dif ~ geo, span=.3)
-#  lines(x, predict(ml, data.frame(geo = x)), col='red', lwd=2)
-#} else {
-  step <- max(geo) / 50.0
-  g1 <- rep(NA, 50)
-  d1 <- rep(NA, 50)
-  n <- 0
-  for (i in 1:50) {
-    idx = geo > step * (i - 1) & geo <= step * i
-    if (length(idx[idx]) > 0) {
-      n <- n + 1
-      g1[n] <- mean(geo[idx])
-      d1[n] <- mean(dif[idx])
-    }
-  }
   lines(g1, d1, col='red', lwd=2)
-#}
 
-lines(x, a + b * x / (c + x),  col='blue',  lwd=2)
+  lines(x, a + b * x / (c + x),  col='blue',  lwd=2)
 
-lines(xrange, c(a, a), col='blue', lwd=.5)
-lines(xrange, c(a + b, a + b), col='blue', lwd=.5)
-lines(xrange, c(a + b/2, a + b/2), col='blue', lwd=.5)
-lines(c(c, c), c(0, a + b/2), col='blue', lwd=.5)
+  lines(xrange, c(a, a), col='blue', lwd=.5)
+  lines(xrange, c(a + b, a + b), col='blue', lwd=.5)
+  lines(xrange, c(a + b/2, a + b/2), col='blue', lwd=.5)
+  lines(c(c, c), c(0, a + b/2), col='blue', lwd=.5)
 
-legend(xrange[2], yrange[1],
-       c(expression(a + b ~~ frac(x, c + x)), 'local'),
-       xjust=1, yjust=0,
-       col=c('blue', 'red'), lwd=2, bty="n")
+  legend(xrange[2], yrange[1],
+         c(expression(a + b ~~ frac(x, c + x)), 'local'),
+         xjust=1, yjust=0,
+         col=c('blue', 'red'), lwd=2, bty="n")
 
-text(xrange[2], a, expression(a), adj=c(1,1.5), col='blue')
-text(xrange[2], a + b/2, expression(a + b / 2), adj=c(1,1.5), col='blue')
-text(0, a + b, expression(a + b), adj=c(0,1.5), col='blue')
-text(c, 0, expression(c),adj=c(-.5,0), col='blue')
+  text(xrange[2], a, expression(a), adj=c(1,1.5), col='blue')
+  text(xrange[2], a + b/2, expression(a + b / 2), adj=c(1,1.5), col='blue')
+  text(0, a + b, expression(a + b), adj=c(0,1.5), col='blue')
+  text(c, 0, expression(c),adj=c(-.5,0), col='blue')
 
-sink('plot01.log')
-summary(m)
-cat('Asymptotic/Actual  R-squared:', cor(dif, predict(m, data.frame(geo = geo)))^2, '\n')
-cat('Asymptotic/Local   R-squared:', cor(d1[1:n], predict(m, data.frame(geo = g1[1:n])))^2, '\n')
-sink()
+  sink('plot01.log')
+  print(summary(m))
+  cat('Asymptotic/Actual  R-squared:', cor(dif, predict(m, data.frame(geo = geo)))^2, '\n')
+  cat('Asymptotic/Local   R-squared:', cor(d1[1:n], predict(m, data.frame(geo = g1[1:n])))^2, '\n')
+  sink()
+
+  return(yrange)
+ 
+}
+
+result <- try(plot01())
+
+if (class(result) == "try-error") {
+  cat(result, file='plot01.log')
+  if (needplot) {
+    plot.new()
+  }
+  del01 <- TRUE
+  yrange <- c(0, max(dif))
+} else {
+  del01 <- FALSE
+  yrange <- result
+}
 
 o <- is.finite(log(geo))
 dif <- dif[o]
@@ -98,7 +117,7 @@ legend(xrange[2], yrange[1],
        col=c('blue', 'red'), lwd=2, bty="n")
 
 sink('plot02.log')
-summary(mlog)
+print(summary(mlog))
 cat('Logarithmic/Actual  R-squared:', cor(dif, predict(mlog, data.frame(geo = geo)))^2, '\n')
 cat('Logarithmic/Local   R-squared:', cor(d1[1:n], predict(mlog, data.frame(geo = g1[1:n])))^2, '\n')
 sink()
@@ -173,3 +192,6 @@ legend(xm, ym,
 
 dev.off()
 
+if (del01) {
+  unlink('plot01.eps')
+}
